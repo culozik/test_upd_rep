b36b3bf883ee84512df2ee4420490d0d60ddc4f1
culozik <eugenbah@gmail.com>
2024-01-15 20:40:26 +0200

    test update 6

--- src/index.js
+++ src/index.js
@@ -28,7 +28,15 @@ const createWindow = () => {
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on("ready", createWindow);
+// app.on("ready", createWindow);
+app.on("ready", () => {
+	createWindow();
+
+	updateApp.updateElectronApp({
+		updateInterval: "5 minutes",
+		notifyUser: true,
+	});
+});
 
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
@@ -49,9 +57,9 @@ app.on("activate", () => {
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and import them here.
-app.on("ready", () => {
-	updateApp.updateElectronApp({
-		updateInterval: "5 minutes",
-		notifyUser: true,
-	});
-});
+// app.on("ready", () => {
+// 	updateApp.updateElectronApp({
+// 		updateInterval: "5 minutes",
+// 		notifyUser: true,
+// 	});
+// });
